2e9f843fb281548dc63549731c5bf35769e81f5d
BM <menonbharath89@gmail.com>
2023-12-18 17:21:40 +0530

    My first commit on Dec17

---
+++ src/Dec17/Lab001.java
@@ -0,0 +1,8 @@
+package src.Dec17;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("My name is Bharath ");
+        System.out.println("Hello World");
+    }
+}
